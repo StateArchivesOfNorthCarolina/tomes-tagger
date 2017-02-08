974f7bc59c27fb0a37592502e7039a4682bae44d
Nitin Arora <nitaro74@gmail.com>
2017-02-08 16:58:28 -0500

    Fixed indentation.

--- html_to_text/html_to_text.js
+++ html_to_text/html_to_text.js
@@ -3,19 +3,19 @@ FILE: html_to_text.js
 
 DESCRIPTION: 
 
-    Converts HTML email file to plain text while retaining hyperlink locations within brackets.
-             
-    Before: "<a href="http://foo.com">bar</a>"
-    After: "bar [http://foo.com/]"
+	Converts HTML email file to plain text while retaining hyperlink locations within brackets.
+			 
+	Before: "<a href="http://foo.com">bar</a>"
+	After: "bar [http://foo.com/]"
 	 
 REQUIREMENTS: PhantomJS <http://phantomjs.org/>
 
 USAGE:
 
-    $ phantomjs .\html_to_text.js
-    >> You must pass a .html file (from your working folder).
-    $ phantomjs .\html_to_text.js test.html
-    >> created file: .\test.html.txt
+	$ phantomjs .\html_to_text.js
+	>> You must pass a .html file (from your working folder).
+	$ phantomjs .\html_to_text.js test.html
+	>> created file: .\test.html.txt
 */
 
 // includes.
@@ -25,8 +25,8 @@ var system = require("system");
 
 // test for file argument.
 if (system.args.length === 1) {
-    console.log("You must pass a .html file (from your working folder).");
-    phantom.exit(); 
+	console.log("You must pass a .html file (from your working folder).");
+	phantom.exit(); 
 } 
 
 // create browser path to HTML file.
@@ -35,24 +35,24 @@ var html = "file:///" + fs.workingDirectory + "/" + system.args[1];
 // rewrite "A" tag values for plain text version.
 var page = webpage.create();
 page.open(html, function (status) {
-    var modify = page.evaluate(function() {
-        var links = document.body.getElementsByTagName("A");
-        for (i=0; i < links.length; i++) {
-            var link = links[i];
+	var modify = page.evaluate(function() {
+		var links = document.body.getElementsByTagName("A");
+		for (i=0; i < links.length; i++) {
+			var link = links[i];
 			if (link.text === "") { // skip if no immediate child text value.
 				continue;
-			}
+		}
 			if (link.href.substr(0,4) !== "http") { // skip if href value doesn't start with "http".
 				continue;
 			}
 			if (link.text === link.href) { // skip if href value equals immediate child text value.
 				continue;
 			}
-            link.innerText = link.innerText + " [" + link.href + "]";
-        }
-        return;        
-    });
-  
+			link.innerText = link.innerText + " [" + link.href + "]";
+		}
+		return;		
+	});
+	
 // write plain text version to file.
 var txt = system.args[1] + ".txt";
 fs.write(txt, page.plainText, "w");
