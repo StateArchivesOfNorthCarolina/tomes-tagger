e09e0d3ae9868ac87a4055fd02181559d6e857ca
Nitin Arora <nitaro74@gmail.com>
2017-02-14 14:30:24 -0500

    Using tempfile as input if hashtag in filename of email.

--- html_to_text/batch.py
+++ html_to_text/batch.py
@@ -1,16 +1,39 @@
-from glob import glob
+import os
+import shutil
 import subprocess
+import tempfile
 import time
+from glob import glob
 
+# make plain text version of given file.
+def convert(htm):
+    if "#" in htm: # PhantomJS can't open files with hashtags, so using temp file.
+        input_handle, input_file = tempfile.mkstemp(suffix=".html")
+        shutil.copy(htm, input_file)
+    else:
+        input_file = htm
+    output_file = htm + ".txt"
+    cmd = "phantomjs html_to_text.js %s %s" %(input_file, output_file)
+    subprocess.call(cmd)
+    if input_file != htm: # delete temp file.
+        os.close(input_handle)
+        os.remove(input_file)
+
+# start timer.
 start = time.time()
 
-html = glob("*.html")
-html_len = len(html)
-
-for htm in html:
-    print htm
-    cmd = "phantomjs html_to_text.js %s" %htm
-    subprocess.call(cmd, shell=True)
-
+# get html files; count number of files; convert files.
+html_len = 0
+for root, dirs, files in os.walk("."):
+    for f in files:
+        f = os.path.join(root, f)
+        if f[len(f) - 5:] == ".html":
+            convert(f)
+            html_len += 1
+    
+# report processing time.
 total_time = time.time() - start
+if html_len == 0:
+    print "No HTML files found."
+    exit()
 print "Processed %d files in %s seconds for an average of %s seconds per file." %(html_len, total_time, total_time/html_len)
