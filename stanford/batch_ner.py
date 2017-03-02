b8966769b42b1531ec7226c834cf77cc5036acad
Nitin Arora <nitaro74@gmail.com>
2017-03-02 13:44:09 -0500

    Simple update to batch_ner.py.

--- stanford/batch_ner.py
+++ stanford/batch_ner.py
@@ -1,21 +1,31 @@
-#!/usr/bin/env python2.7
-
 import glob
 import os
+import sys
 
-DIR = "D:\TOMES\TESTS\html2text" # where the emails are.
-MAPPING = "mappings.txt" # the file with the regexner mapping.
+# get folder to process from command line argument.
+try:
+    inputs = sys.argv[1]
+    outputs = sys.argv[2]
+except:
+    exit("You must pass both input and output folder names.")
 
-emails = glob.glob(DIR + "/*.txt")
-os.chdir("stanford-corenlp-full-2016-10-31")
+# glob all .txt files in @folder.
+texts = glob.glob(inputs + "/*.txt")
 
-i = 1
-for email in emails:
-    cmd = """java -cp "*" -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,regexner \
--file %s -regexner.mapping ../%s -outputDirectory %s -outputFormat json""" %(email, MAPPING, DIR)
-    print
-    print "*"*10
-    print i, ": ", cmd
-    os.system(cmd)
+# run CoreNLP on all files.
+i = 0
+for text in texts:
+    json = outputs + "/" + os.path.basename(text) + ".json"
+    if os.path.isfile(json): # skip if output JSON file already exists.
+        continue
+    cmd = """java -cp "stanford-corenlp-full-2016-10-31/*" -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,regexner -file {} -regexner.mapping mappings.txt -outputDirectory {} -outputFormat json"""
+    cmd = cmd.format(text, outputs)
+    #print(cmd)
+    try:
+        os.system(cmd)
+    except:
+        pass
+    if i > 9999:
+        exit()
     i += 1
     #break
