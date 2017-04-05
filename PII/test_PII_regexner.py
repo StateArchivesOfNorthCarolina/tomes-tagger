3b6601a7de2cebf4687b7f9f34bc93207e5d88d4
Nitin Arora <nitaro74@gmail.com>
2017-04-05 10:24:48 -0400

    Updated testing script, added results tsv file.

--- PII/test_PII_regexner.py
+++ PII/test_PII_regexner.py
@@ -1,61 +1,134 @@
 #!/usr/bin/env python3
 
-# import modules
+# import modules.
 import os
 from glob import glob
 from pycorenlp import StanfordCoreNLP
 
-# CoreNLP server must be running a la:
-# java -mx2g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000
-nlp = StanfordCoreNLP("http://localhost:9000")
+# set CoreNLP port.
+# note: CoreNLP server must be running a la: 
+# "$ java -mx2g -cp "*" edu.stanford.NLP.pipeline.StanfordCoreNLPServer -port 9000".
+NLP = StanfordCoreNLP("http://localhost:9000")
 
-def getNER(text, PII_type):
+def getNER(phrase, PII_type):
+    """ Gets NER tags for given @phrase and report if it matches @PII_type.
+    
+    Args:
+        phrase (str): The text to apply NLP to.
+        PII_type: The label for the type of PII ("PII.beacon_id", etc.).
+    
+    Returns:
+        tuple: 
+            - retext (str): the joined tokens analyzed by CoreNLP (str).
+            - ner_tags (list): the list of NER tags found.
+            - ner_test (bool): if the only NER tag found equals @PII_type.
+            - accuracy (decimal): the ratio of tokens tagged as PII_type divided by the total
+              number of tokens analyzed.
+
+    Examples:
+        >>> getNER("foo@bar.com", "PII.email_address")
+        ('foo@bar.com', ['PII.email_address'], True, 1.0)
+    """
+
+    # set RegexNER mapping file.
     mapping = "regexner_TOMES/" + PII_type + "__regexnerMappings.txt"
+    
+    # run CoreNLP on @phrase with @mapping file.
     properties = {"annotators":"tokenize, ssplit, pos, ner, regexner",
                  "outputFormat":"json",
                  "regexner.mapping":mapping}
-    output = nlp.annotate(text, properties=properties)
+    output = NLP.annotate(phrase, properties=properties)
+    #print(output)
+    
+    # get tokens from output.
     tokens = output["sentences"][0]["tokens"]
     
-    words = [o["word"] for o in tokens]
-    word = " ".join(words)
+    # get words from @tokens; re-join into single string.
+    word = [o["word"] for o in tokens]
+    rephrase = " ".join(word)
     
-    ners = [o["ner"] for o in tokens]
-    pii = [n for n in ners if n != "O"]
-    ner = list(set(ners))
-    ner_test = ner == [PII_type] 
-
-    accuracy = len(pii)/len(tokens)
-    return (word, ner, ner_test, accuracy)
-
-#
-header = ["PII_type", "Text", "NER", "NER_passed", "Accuracy", "Match", "Pass"]
-print("\t".join(header))
-PII = glob("*/")
-for P in PII:
-    P = P[:-1]
-    os.chdir(P)
-    test_data = open(P + "__testData.txt", "r")
-    match_data = open(P + "__matchData.txt", "r")
-    test_data = test_data.read().split("\n")
-    match_data = match_data.read().split("\n")
-    #print(test_data)
-
-    for text in test_data:
-        word, ner, ner_test, accuracy = getNER(text, P)
-        match = word in match_data
-        if ner_test and match:
-            passed = True
-        elif ner_test and not match:
-            passed = False
-        elif not ner_test and match:
-            passed = False
-        elif not ner_test and not match:
-            passed = True
-        report = [P, word, ner, ner_test, accuracy, match, passed]
-        report = [str(r) for r in report]
-        report = "\t".join(report)
-        print(report)
-    break
+    # get non-empty NER tags; prepare report data.
+    ner = [o["ner"] for o in tokens]
+    ner_tags = [n for n in ner if n != "O"]
+    ner_tags = list(set(ner_tags))
+    ner_test = ner_tags == [PII_type] 
+    accuracy = len(ner_tags)/len(tokens)
     
+    return (rephrase, ner_tags, ner_test, accuracy)
+
+
+def testPII():
+    """ Tests CoreNLP RegexNER PII mappings against test and match data;
+        reports resulst as tab-delimied data.
+    """
     
+    # create empty output list.
+    tsv = []
+
+    # append @tsv header.
+    header = ["PII_type", "Test_Data", "NER_tags", "isCorrectNER", "NER_Accuracy",
+             "isMatchData", "isTestPassed"]
+    header = "\t".join(header)
+    tsv.append(header)
+
+    # glob all PII folders.
+    piis = glob("PII.*/")
+
+    # for each PII type; run getNER() on all test data.
+    for pii in piis:
+        
+        # navigate to PII folder.
+        pii = pii[:-1]
+        os.chdir(pii)
+        
+        # open/read test and match data files.
+        test_data = open(pii + "__testData.txt")
+        match_data = open(pii + "__matchData.txt")
+        test_data = test_data.read().split("\n")
+        match_data = match_data.read().split("\n")
+
+        # runs tests; determine if each test passed or not; append results to @tsv.
+        for line in test_data:
+            
+            # skip blank lines as they will break getNER().
+            if line == "":
+                continue
+
+            # run test; determine if test passed.
+            phrase, ner_tags, ner_test, accuracy = getNER(line, pii)
+            match = phrase in match_data
+            if ner_test and match:
+                passed = True
+            elif ner_test and not match:
+                passed = False
+            elif not ner_test and match:
+                passed = False
+            elif not ner_test and not match:
+                passed = True
+            
+            # append test data to @tsv.
+            test = [pii, phrase, ner_tags, ner_test, accuracy, match, passed]
+            test = [str(r) for r in test]
+            test = "\t".join(test)
+            tsv.append(test)
+        
+        # go back up one directory.
+        os.chdir("..")
+        #break
+
+    # convert list to tsv data.
+    tsv = "\n".join(tsv)
+    return tsv
+        
+### let's test ...
+SCREEN = False
+def main():
+    results = testPII()
+    if SCREEN == True:
+        print(results)
+    else:
+        with open("test_PII_regexner.tsv", "w") as f:
+            f.write(results)
+
+if __name__ == "__main__":
+    main()
