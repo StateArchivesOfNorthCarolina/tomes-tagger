d8d71a0f1ff4c1934a7e1f6e3fd643189de647b8
Nitin Arora <nitaro74@gmail.com>
2017-04-20 11:07:17 -0400

    Added explicit param to tell method to convert HTML file OR HTML string.

--- html_to_text/html_to_text.py
+++ html_to_text/html_to_text.py
@@ -2,10 +2,7 @@
 
 """
 TODO:
-    - I still get a Windows/"file in use" error every now and then even with the __del__
-    method. Maybe I should just remove that method and let the file stick around?
-    Alternatively, create an self.array of used Temporary Files and delete each one with
-    __del__()?
+    - ???
 """
 
 # import modules.
@@ -21,59 +18,61 @@ class Lynx():
     """ A class to convert HTML files OR strings to plain text via the Lynx browser.
     For more information about Lynx, see: http://lynx.browser.org. """
     
-    def __init__(self, custom_options={}):
+    def __init__(self, custom_options={}, temp_file="_tmp.html"):
 
         """ Sets instance attributes.
         
         Args:
-            custom_options: ???
+            custom_options (dict): 
+            temp_file (str): 
         """
 
-        # default options for Lynx.
+        # set default options for Lynx.
         options = {"nolist":True, "nomargins":True}
         
-        # add custom options.
+        # add in custom options.
         for key, val in custom_options.items():
             options[key] = val
         self.options = options
         self.options = [key for key, val in options.items() if val]
         self.options.append("dump")
 
-        # persistent temporary file name.
-        self.html_file = "_tmp.html"
+        # set persistent temporary file name.
+        self.temp_file = temp_file
 
 
     def __del__(self):
 
+        """ Trys to remove temporary file if it exists. Passes on permission error. """
+        if os.path.isfile(self.temp_file):
+            try:
+                os.remove(self.temp_file)
+            except PermissionError:
+                pass
 
-        """ Removes temporary file if it exists. """
-        pass
-        #if os.path.isfile(self.html_file):
-        #    os.remove(self.html_file)
 
-    
-    def to_text(self, html, charset="utf-8"):
+    def to_text(self, html, is_raw=False, charset="utf-8"):
 
         """ Converts HTML files OR strings to plain text via the Lynx browser.
 
         Args:
             html (str):
+            is_raw (bool):
             charset (str):
 
         Examples:
 
         """
-
+    
         # create beginning Lynx command line snippet.
         args = "lynx -"
         args += " -".join(self.options)
 
-        # if @html is not a file, write data to temporary file.
-        # complete Lynx command line snippet.
-        if not os.path.isfile(html):
-            with codecs.open(self.html_file, "w", encoding=charset) as tmp:
+        # if @html is an HTML string, write data to temporary file.
+        if is_raw:
+            with codecs.open(self.temp_file, "w", encoding=charset) as tmp:
                 tmp.write(html)
-            args += " " + self.html_file
+            args += " " + self.temp_file
         else:
             args += " " + html
 
@@ -88,12 +87,13 @@ class Lynx():
 
         return stdout
 
-#
+
 def main():
     lynx = Lynx(custom_options={})
     print(lynx.to_text("testLists.html"))
-    print(lynx.to_text("<ul><li>foo</li></ul>"))
-    print(lynx.to_text("testLists.html"))
+    print("-----")
+    print(lynx.to_text("<ul><li>foo</li></ul>", is_raw=True))
+
 
 if __name__ == "__main__":
     main()
