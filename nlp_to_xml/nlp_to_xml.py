8a2353f8bbe9df5c56c386937888f65c4eb34832
Nitin Arora <nitaro74@gmail.com>
2017-04-26 11:05:44 -0400

    Changed schema to be more attribute centric (less verbose).

--- nlp_to_xml/nlp_to_xml.py
+++ nlp_to_xml/nlp_to_xml.py
@@ -4,7 +4,7 @@
 This module converts Stanford CoreNLP JSON output to XML per the ./tagged_content.xsd schema.
 
 TODO:
-    - You need an external, canonical data source for the custom NER tags.
+    - Need an external, canonical data source for the custom NER tags, perhaps a SKOS file.
 """
 
 # import modules.
@@ -32,7 +32,7 @@ class JsonToXML():
                           "PII.social_security_number"]
 
 
-    def xml(self, jdoc, is_raw=False, charset="utf-8", as_string=True):
+    def xml(self, jdoc, is_raw=False, charset="utf-8", as_string=True, beautify=True):
         """ """
 
         # if @is_raw == False, read JSON file.
@@ -52,46 +52,44 @@ class JsonToXML():
         x_tokens = etree.Element(ns_prefix + "tokens", nsmap=ns_map)
         
         # parse JSON; write XML.
-        j_sentences = jsml["sentences"]
-        for j_sentence in j_sentences:
+        sentences = jsml["sentences"]
+        for sentence in sentences:
         
-            j_tokens = j_sentence["tokens"]
-            for j_token in j_tokens:
-
-                x_token = etree.SubElement(x_tokens, ns_prefix + "token", nsmap=ns_map)
+            tokens = sentence["tokens"]
+            for token in tokens:
                 
-                j_originalText = j_token["originalText"]
-                j_ner = j_token["ner"]
-                j_after = j_token["after"]
-
-                x_text = etree.SubElement(x_token, ns_prefix + "text", nsmap=ns_map)
-                x_text.text = j_originalText
-
-                if j_ner != "O":
-
-                    x_entity = etree.SubElement(x_token, ns_prefix + "entity", nsmap=ns_map)
-                    
-                    if j_ner in self.custom_ner:
-                        x_authority = "ncdcr.gov"
+                #
+                originalText = token["originalText"]
+                ner = token["ner"]
+                after = token["after"]
+                
+                #
+                x_token = etree.SubElement(x_tokens, ns_prefix + "token", nsmap=ns_map)
+                x_token.text = originalText
+                x_token.set("suffix", after)
+    
+                # 
+                if ner != "O":
+ 
+                    if ner in self.custom_ner:
+                        authority = "ncdcr.gov"
                     else:
-                        x_authority = "stanford.edu"
+                        authority = "stanford.edu"
 
-                    x_entity.text = j_ner
-                    x_entity.set("authority", x_authority)
-
-                x_separator = etree.SubElement(x_token, ns_prefix + "separator",
-                                              nsmap=ns_map)
-                x_separator.text = j_after
-
+                    x_token.set("NER", ner)
+                    x_token.set("NER.authority", authority)
+                
+        #
         if as_string:
-            x_tokens = etree.tostring(x_tokens)
+            x_tokens = etree.tostring(x_tokens, pretty_print=beautify)
+            x_tokens = x_tokens.decode(charset)
         return x_tokens
 
 
 # TEST.
 def main():
-    j2h = JsonToXML()
-    x = j2h.xml("sample.json", as_string=False)
+    j2x = JsonToXML()
+    x = j2x.xml("sample.json", as_string=True)
     return x
 
 if __name__ == "__main__":
