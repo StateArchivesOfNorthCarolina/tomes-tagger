c309b63f0945753fa4ee751ed04c17ecf6717612
Nitin Arora <nitaro74@gmail.com>
2017-05-23 14:13:35 -0400

    Fleshed out a litte more.

--- lib/mets/fileSec.py
+++ lib/mets/fileSec.py
@@ -1,43 +1,62 @@
-import datetime
+"""
+attachments = fileSec.fileGrp(directory="./myDir", id_prefix="attachments_")
+attachments.xml("attachments")
+"""
+
+#
 import hashlib
 import os
+from datetime import datetime
 
-def attachments(r):
-    ID = None
-    CHECKSUM = None
-    CHECKSUMTYPE = "SHA-256"
-    CREATED = None
-    MIMETYPE = "attachment/xml"
-    SIZE = None
 
-    files_dict = {}
+#
+def get_mime(f):
+    mimes = {".xml": "application/xml"}
+    ext = os.path.splitext(f)[1]
+    mime = mimes.get(ext)
+    return mime
+
+
+#
+def fileGrp(directory, id_prefix):
+    
     i = 0
-    for root, dirs, files in os.walk(r + "/attachments"):
+    fileGrp = {}
+    for root, dirs, files in os.walk(directory + "/attachments"):
         
-        for name in files:
-            name = os.path.join(root, name)
-            
-            ID = "attachment_" + str(i).zfill(5)
-            
-            with open(name):
-                m = hashlib.sha256()
-                fr = open(name, "rb").read()
-                m.update(fr)
-                CHECKSUM = m.hexdigest()
-            
-            SIZE = os.path.getsize(name)
-            CREATED = os.path.getctime(name)
+        for myfile in files:
+
+            flocat = os.path.join(root, myfile)
+            with open(flocat, "rb") as f:
+                checksum = hashlib.sha256()
+                checksum.update(f.read())
+                file_checksum = checksum.hexdigest()
 
             # "The number of seconds can include decimal digits to arbitrary precision." per: https://www.w3.org/TR/xmlschema-2/
-            CREATED = datetime.datetime.utcfromtimestamp(CREATED).isoformat() 
+            file_created = os.path.getctime(flocat)
+            file_created = datetime.utcfromtimestamp(file_created).isoformat() 
             
-            files_dict[name] = {"ID": ID,
-                                "CHECKSUM": CHECKSUM,
-                                "CREATED": CREATED,
-                                "SIZE": SIZE}
-            break
+            flocat_loctype = flocat.replace(directory, "")
+            flocat_loctype = flocat_loctype.replace("\\", "/")
+            fileGrp[flocat] = {"file":
+                                    {"@ID": id_prefix + str(i).zfill(5),
+                                    "@CHECKSUM": file_checksum,
+                                    "@CHECKSUMTYPE": "SHA-256",
+                                    "@CREATED": file_created,
+                                    "@MIMETYPE": get_mime(flocat),
+                                    "@SIZE": os.path.getsize(flocat),
+                                    "FLocat": 
+                                        ({"@LOCTYPE": "OTHER"},
+                                        flocat_loctype)
+                                    }
+                                }
+            i += 1
+            #break
 
-        return files_dict
-        
-a = attachments()
-print(a)
+        return fileGrp
+   
+testDir = open(".testDir").read()
+results = fileGrp(testDir, "attachments_")
+import json
+results = json.dumps(results, indent=2)
+print(results)
