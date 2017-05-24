7cafa4b6e4f1faac8a31c45a542c637fdd93b644
Nitin Arora <nitaro74@gmail.com>
2017-05-24 14:01:55 -0400

    Cleaned up code. Fairly usable now.

--- lib/mets/fileSec.py
+++ lib/mets/fileSec.py
@@ -1,65 +1,77 @@
 """
-attachments = fileSec.fileGrp("./myDir", "attachments")
+usage:
+    >>> make_fileSec("./myDir")
 """
 
-#
+# import modules.
 import hashlib
+import mimetypes
 import os
 from datetime import datetime
 from lxml import etree
 
-#
-def get_mime(f):
-    mimes = {".xml": "application/xml"}
-    ext = os.path.splitext(f)[1]
-    mime = mimes.get(ext)
-    return mime
-    
-#
-def fileGrp(directory, suffix):
+# set namespace prefixes.
+ns_url = "http://www.loc.gov/METS/" # get this from an external source eventually OR when inhereted.
+ns_dir_id = "{" + ns_url + "}"
+ns_map = {None : ns_url}
 
-    ns_url = "http://www.loc.gov/METS/" # get this from an external source eventually OR when inhereted.
-    ns_prefix = "{" + ns_url + "}"
-    ns_map = {"METS" : ns_url}
-    
-    i = 0
-    #fileGrp = []
-    fileGrp_el = etree.Element(ns_prefix + "fileGrp", nsmap=ns_map)
-    fileGrp_el.set("ID", suffix)
-    for root, dirs, files in os.walk(directory + "/" + suffix):
+
+def make_fileSec(filepath):
+    """ Returns METS <fileSec> for all files in @filepath.
+    Return type is <class 'lxml.etree._Element'>. """
+
+    # create root <fileSec> element.
+    fileSec_el = etree.Element(ns_dir_id + "fileSec", nsmap=ns_map)
+
+    # loop through subdirectories and files.
+    for dirpath, dirnames, filenames in os.walk(filepath):
         
-        for myfile in files:
+        # get current relative path; replace Windows backslashes.
+        dir_id = os.path.relpath(dirpath, filepath)
+        dir_id = dir_id.replace("\\", "/")
+        
+        # create <fileGrp> element for current directory.
+        fileGrp_el = etree.SubElement(fileSec_el, ns_dir_id + "fileGrp", nsmap=ns_map)
+        fileGrp_el.set("ID", dir_id)
 
-            myfile = os.path.join(root, myfile)
+        for filename in filenames:  
 
-            file_el = etree.SubElement(fileGrp_el, ns_prefix + "file", nsmap=ns_map)
+            # create full path to file.
+            fullname = os.path.join(dirpath, filename)
 
-            file_el.set("ID", suffix + "_" + str(i).zfill(5))
+            # create <file> element for current file; set easy attributes.
+            file_el = etree.SubElement(fileGrp_el, ns_dir_id + "file", nsmap=ns_map)
             file_el.set("CHECKSUMTYPE", "SHA-256")
-            file_el.set("MIMETYPE", get_mime(myfile))
-            file_el.set("SIZE", str(os.path.getsize(myfile)))
-            with open(myfile, "rb") as f:
+            file_el.set("SIZE", str(os.path.getsize(fullname)))
+
+            # set MIMETYPE attribute.
+            mime = mimetypes.guess_type(fullname)[0]
+            if mime is not None:
+                file_el.set("MIMETYPE", mime)
+            
+            # set CHECKSUM attribute.
+            with open(fullname, "rb") as f:
                 checksum = hashlib.sha256()
                 checksum.update(f.read())
                 file_el.set("CHECKSUM", checksum.hexdigest())
 
-            # "The number of seconds can include decimal digits to arbitrary precision." per: https://www.w3.org/TR/xmlschema-2/
-            file_created = os.path.getctime(myfile)
+            # set CREATED attribute.
+            file_created = os.path.getctime(fullname)
             file_created = datetime.utcfromtimestamp(file_created).isoformat()
             file_el.set("CREATED", file_created)
 
-            flocat_el = etree.SubElement(file_el, ns_prefix + "FLocat", nsmap=ns_map)
+            # create <FLocat> element; set LOCTYPE attribute.
+            flocat_el = etree.SubElement(file_el, ns_dir_id + "FLocat", nsmap=ns_map)
             flocat_el.set("LOCTYPE", "OTHER")
-            flocat_el.text = myfile.replace(directory, "")
-            flocat_el.text = flocat_el.text.replace("\\", "/")
+            flocat_el.text = filename
 
-            i += 1
-            if i == 5:
-                break
+    return fileSec_el
 
-        return fileGrp_el
-   
+
+# TEST.
 testDir = open(".testDir").read()
-results = fileGrp(testDir, "attachments")
-xml = etree.tostring(results, pretty_print=True)
-print(xml.decode("utf-8"))
+fileSec = make_fileSec(testDir)
+fileSec = etree.tostring(fileSec, pretty_print=True)
+fileSec = fileSec.decode("utf-8")
+print(fileSec)
+
