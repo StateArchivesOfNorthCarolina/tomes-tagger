15576d42876acc88302fab8e3c09ce7c31888d84
Nitin Arora <nitaro74@gmail.com>
2017-05-25 11:17:25 -0400

    Now fileGrp IDs are hashes; move namespace stuff into external file.

--- lib/mets/fileSec.py
+++ lib/mets/fileSec.py
@@ -1,54 +1,54 @@
+#!/usr/bin/env python3
+
 """
-This module creates a METS <fileSec> tree for a given user-inputted path.
-The output can be integrated into a complete METS file.
+This module creates a METS <fileSec> tree for a given user-inputted path. The output can be
+integrated into a complete METS file.
 
 Usage:
-    >>> make_fileSec("./myDir")
+    >>> import fileSec
+    >>> mets_fileSec = fileSec.make("./myDir") # etree <fileSec> element.
+    >>> mets_fileSec_ids = fileSec.file_ids # list of <file> element @ID attributes.
 
 TODO:
-    - Get namespace stuff from an external source eventually.
-    - You'll need to add the file@ID attribute back so you can reference it in the
-    structMap section. It might be best to just use a zero-padded int for all
-    files. That way the structMap sequence is intuitive.
-    - Need a separate fileGrp for preservation EAXS vs. tagged. This can wait
-    until our AIP structure is solid.
+    - Might need a separate <fileGrp> element for preservation EAXS vs. tagged. This can wait until
+    our AIP structure is solid. It's realy up to the Archivists.
+    - Also, the <fileGrp> @USE attribute might be something they want too.
 """
 
 # import modules.
-import hashlib
+import mets
 import mimetypes
 import os
 from datetime import datetime
+from hashlib import sha1, sha256
 from lxml import etree
 
-# set namespace prefixes.
-ns_url = "http://www.loc.gov/METS/"
-ns_dir_id = "{" + ns_url + "}"
-ns_map = {None : ns_url}
 
 # to save <file> ID attributes.
 file_ids = []
 
-def make_fileSec(filepath):
+
+def make(filepath):
     """ Returns METS <fileSec> for all files in @filepath.
     Return type is <class 'lxml.etree._Element'>. """
-
     
     i = 0
 
     # create root <fileSec> element.
-    fileSec_el = etree.Element(ns_dir_id + "fileSec", nsmap=ns_map)
+    fileSec_el = etree.Element(mets.ns_id + "fileSec", nsmap=mets.ns_map)
 
     # loop through subdirectories and files.
-    for dirpath, dirnames, filenames in os.walk(filepath): 
+    for dirpath, dirnames, filenames in os.walk(filepath):
         
         # get current relative path; replace Windows backslashes.
-        dir_id = os.path.relpath(dirpath, filepath)
-        dir_id = dir_id.replace("\\", "/")
+        relative_path = os.path.relpath(dirpath, filepath)
+        relative_path = relative_path.replace("\\", "/")
         
         # create <fileGrp> element for current directory.
-        fileGrp_el = etree.SubElement(fileSec_el, ns_dir_id + "fileGrp", nsmap=ns_map)
-        fileGrp_el.set("ID", dir_id)
+        fileGrp_el = etree.SubElement(fileSec_el, mets.ns_id + "fileGrp", nsmap=mets.ns_map)
+        relative_path_bytes = bytes(relative_path, encoding="utf-8")
+        fileGrp_id = sha1(relative_path_bytes).hexdigest()
+        fileGrp_el.set("ID", fileGrp_id)
 
         for filename in filenames:  
 
@@ -56,7 +56,7 @@ def make_fileSec(filepath):
             fullname = os.path.join(dirpath, filename)
 
             # create <file> element for current file; set easy attributes.
-            file_el = etree.SubElement(fileGrp_el, ns_dir_id + "file", nsmap=ns_map)
+            file_el = etree.SubElement(fileGrp_el, mets.ns_id + "file", nsmap=mets.ns_map)
             file_el.set("CHECKSUMTYPE", "SHA-256")
             file_el.set("SIZE", str(os.path.getsize(fullname)))
 
@@ -72,9 +72,8 @@ def make_fileSec(filepath):
             
             # set CHECKSUM attribute.
             with open(fullname, "rb") as f:
-                checksum = hashlib.sha256()
-                checksum.update(f.read())
-                file_el.set("CHECKSUM", checksum.hexdigest())
+                checksum = sha256(f.read()).hexdigest()
+                file_el.set("CHECKSUM", checksum)
 
             # set CREATED attribute.
             file_created = os.path.getctime(fullname)
@@ -82,9 +81,9 @@ def make_fileSec(filepath):
             file_el.set("CREATED", file_created)
 
             # create <FLocat> element; set LOCTYPE attribute.
-            flocat_el = etree.SubElement(file_el, ns_dir_id + "FLocat", nsmap=ns_map)
+            flocat_el = etree.SubElement(file_el, mets.ns_id + "FLocat", nsmap=mets.ns_map)
             flocat_el.set("LOCTYPE", "OTHER")
-            flocat_el.text = filename
+            flocat_el.text = etree.CDATA(relative_path + "/" + filename)
 
             i += 1
 
@@ -92,9 +91,21 @@ def make_fileSec(filepath):
 
 
 # TEST.
-testDir = open(".testDir").read()
-fileSec = make_fileSec(testDir)
-fileSec = etree.tostring(fileSec, pretty_print=True)
-fileSec = fileSec.decode("utf-8")
-print(fileSec)
-print(file_ids)
+def main(path):
+
+    # create <fileSec> based on @path.
+    fileSec = make(path)
+
+    # print XML.
+    fileSec = etree.tostring(fileSec, pretty_print=True)
+    fileSec = fileSec.decode("utf-8")
+    print(fileSec)
+
+    # print list of <file> @ID attributes.
+    print(file_ids)
+
+if __name__ == "__main__":
+    
+    import plac
+    plac.call(main)
+
---
+++ lib/mets/namespace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" This file contains constant METS namespace values for import by other modules. """
+
+ns_prefix = "mets"
+ns_url = "http://www.loc.gov/METS/"
+ns_id = "{" + ns_url + "}"
+ns_map = {None : ns_url}
+
