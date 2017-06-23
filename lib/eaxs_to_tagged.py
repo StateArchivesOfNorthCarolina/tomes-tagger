e84b7129dd2572510d1249f8d567d1041994b046
Nitin Arora <nitaro74@gmail.com>
2017-06-23 10:47:13 -0400

    More or less final interface draft.

--- lib/eaxs_to_tagged.py
+++ lib/eaxs_to_tagged.py
@@ -30,7 +30,7 @@ class EAXSToTagged():
     """
 
 
-    def __init__(self, eaxs_file, tagged_eaxs_file, html_stripper, nlp_tagger,
+    def __init__(self, eaxs_file, tagged_eaxs_file, strip_html, nlp_tagger,
             charset="utf-8"):
         """ Sets instance attributes.
         
@@ -43,52 +43,21 @@ class EAXSToTagged():
         # set attributes.
         self.eaxs_file = eaxs_file
         self.tagged_eaxs_file = tagged_eaxs_file
-        self.html_stripper = html_stripper
+        self.strip_html = strip_html
         self.nlp_tagger = nlp_tagger
         self.charset = charset
         
         # set namespace attributes.
-        self.ncdcr_prefix = "ncdcr"
         self.ncdcr_uri = "http://www.archives.ncdcr.gov/mail-account"
-        self.ns_map  = {self.ncdcr_prefix:self.ncdcr_uri,
+        self.ns_map  = {"ncdcr":self.ncdcr_uri,
                 "xsi":"http://www.w3.org/2001/XMLSchema-instance"}
-    
-
-    def _get_root(self):
-        """ Gets root "Account" element ... """
-        
-        pass
 
     
-    def _get_globalID(self):
-        """ """
-        
-        pass
-    
-
-    def _get_folders(self):
-        """ Gets all <Folder> elements in self.eaxs_file. 
-        
-        Returns:
-            <class 'lxml.etree.iterparse'>
-        """
-        
-        eaxs_file = self.eaxs_file
-        ncdcr_uri = self.ncdcr_uri
-        
-        # get element generator. 
-        folder_el = "{" + ncdcr_uri + "}Folder"
-        folders = etree.iterparse(eaxs_file, events=("end",), tag=folder_el,
-                strip_cdata=False)
-        
-        return folders
-
-    
-    def _get_messages(self, folder):
+    def _get_messages(self, folder_el):
         """ Gets all <Message> elements from an EAXS <Folder> element. 
         
         Args:
-            - folder (lxml.etree._Element): An EAXS <Folder> etree element.
+            - folder_el (lxml.etree._Element): An EAXS <Folder> etree element.
 
         Returns:
             <class 'lxml.etree.ElementChildIterator'>
@@ -98,22 +67,21 @@ class EAXSToTagged():
         
         # get element generator.
         message_el = "{" + ncdcr_uri + "}Message"
-        messages =  folder.iterchildren(tag=message_el)
+        messages =  folder_el.iterchildren(tag=message_el)
         
         return messages
 
 
-    def _update_message(self, message):
+    def _update_message(self, message_el):
         """
         
         Args:
-            - message (lxml.etree._Element):
+            - message_el (lxml.etree._Element):
         """
 
         tagged_eaxs_file = self.eaxs_file
-        ncdcr_prefix = self.ncdcr_prefix
         ns_map = self.ns_map
-        html_stripper = self.html_stripper
+        strip_html = self.strip_html
         nlp_tagger = self.nlp_tagger
         charset = self.charset
 
@@ -121,57 +89,72 @@ class EAXSToTagged():
         _singleBody = "ncdcr:MultiBody/ncdcr:SingleBody[1]/"
 
         # elements to alter.
-        #message_id = message.xpath("ncdcr:MessageId/text()", namespaces=ns_map)
-        charset_el = message.xpath(_singleBody + "ncdcr:Charset[1]", namespaces=ns_map)[0]
-        content_el = message.xpath(_singleBody + "ncdcr:BodyContent/ncdcr:Content[1]",
+        message_id = message_el.xpath("ncdcr:MessageId/text()", namespaces=ns_map)
+        print(message_id)
+
+        charset_el = message_el.xpath(_singleBody + "ncdcr:Charset[1]", namespaces=ns_map)[0]
+        content_el = message_el.xpath(_singleBody + "ncdcr:BodyContent/ncdcr:Content[1]",
                 namespaces=ns_map)[0]
-        content_type_el = message.xpath(_singleBody + "ncdcr:ContentType[1]",
+        content_type_el = message_el.xpath(_singleBody + "ncdcr:ContentType[1]",
                 namespaces=ns_map)[0]
-        transfer_encoding_el = message.xpath(_singleBody + "ncdcr:TransferEncoding[1]",
+        transfer_encoding_el = message_el.xpath(_singleBody + "ncdcr:TransferEncoding[1]",
                 namespaces=ns_map)
 
         # stop if no content.
         if content_el != None:
             if content_el.text == None:
-                return message
+                return message_el
 
         # decode Base64 if needed.
         if len(transfer_encoding_el) != 0:
             if transfer_encoding_el[0].text == "base64":
                 text = base64.b64decode(content_el.text)
-                content_el.text = text.decode(encoding=charset, errors="backslashreplace")
-        
+                text = text.decode(encoding=charset, errors="backslashreplace")
+                content.text = etree.CDATA(text)
+            # getting ValueError here, apparently not XML compatible for message: ['<7C7C3D5653AD7743872A90E300B6519C18CA96852A@NCWITMXMBEV44.ad.ncmail>']
+
         # alter element values.
         charset_el.text = charset
         if content_type_el.text == "text/html":
-            content_el.text = html_stripper(content_el.text)
+            content_el.text = strip_html(content_el.text)
         content_el.text = nlp_tagger(content_el.text)
         content_type_el.text = "text/xml"
         transfer_encoding_el = None
         
-        return message
+        return message_el
 
 
     def tag_eaxs(self):
-        """ Writes file ... """
+        """ Writes file ... return filepath ... """
 
-        pass
+        eaxs_file = self.eaxs_file
+        ncdcr_uri = self.ncdcr_uri
+
+        #
+        account = "{" + ncdcr_uri + "}Account"
+        account_el = etree.Element(account)
+
+        #
+        root = etree.iterparse(eaxs_file, tag=account, strip_cdata=False)
+        root = next(root)[1]
+        
+        children = root.iterchildren()
+        for child_el in children:
+            if child_el.tag == "{" + ncdcr_uri + "}Folder":
+                messages = self._get_messages(child_el)
+                for message_el in messages:
+                    message_el = self._update_message(message_el)
+            account_el.append(child_el)
+
+        return account_el
 
 
 # TEST.
 def main(eaxs_file, tagged_eaxs_file):
 
     e2t = EAXSToTagged(eaxs_file, tagged_eaxs_file,  lambda x: x, lambda x: "FOOBAR")
-    folders = e2t._get_folders()
-    for event, folder in folders:
-        messages = e2t._get_messages(folder)
-        for message in messages:
-            messagex = e2t._update_message(message)
-            messagex = etree.tostring(messagex, pretty_print=True)
-            messagex = messagex.decode()
-            print(messagex)
-            break
-        folder.clear()
+    tagged = e2t.tag_eaxs()
+    print(tagged)
 
 
 if __name__ == "__main__":
