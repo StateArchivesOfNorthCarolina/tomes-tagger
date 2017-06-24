86317a990f5775c822588ce677e20591c1491f1d
Nitin Arora <nitaro74@gmail.com>
2017-06-23 21:30:37 -0400

    Fixed string format bug.

--- lib/eaxs_to_tagged.py
+++ lib/eaxs_to_tagged.py
@@ -37,19 +37,15 @@ class EAXSToTagged():
     """
 
 
-    def __init__(self, eaxs_file, tagged_eaxs_file, strip_html, nlp_tagger,
-            charset="utf-8"):
+    def __init__(self, strip_html, nlp_tagger, charset="utf-8"):
         """ Sets instance attributes.
         
         Args:
-            - eaxs_file (str): Path to EAXS file.
             - charset (str): Optional encoding with which to open @eaxs_file.
             ???
         """
 
         # set attributes.
-        self.eaxs_file = eaxs_file
-        self.tagged_eaxs_file = tagged_eaxs_file
         self.strip_html = strip_html
         self.nlp_tagger = nlp_tagger
         self.charset = charset
@@ -75,7 +71,7 @@ class EAXSToTagged():
         # get element generator.
         message_el = "{" + ncdcr_uri + "}Message"
         messages =  folder_el.iterchildren(tag=message_el)
-        
+ 
         return messages
 
 
@@ -84,10 +80,10 @@ class EAXSToTagged():
 
         ncdcr_uri = "{" + self.ncdcr_uri + "}"
     
-        bodyContent = "{ncdcr_uri}MultiBody/{ncdcr_uri}SingleBody[1]/"
-        bodyContent += "{ncdcr_uri}BodyContent/{ncdcr_uri}{tag}[1]"
-        
-        el_path = bodyContent.format(ncdcr_uri=ncdcr_uri, tag=tag)
+        el_path = "{ncdcr_uri}MultiBody/{ncdcr_uri}SingleBody[1]/"
+        el_path += "{ncdcr_uri}BodyContent/{ncdcr_uri}{tag}[1]"
+        el_path = el_path.format(ncdcr_uri=ncdcr_uri, tag=tag)
+
         elems = message_el.find(el_path)
         
         el, el_text = None, value
@@ -104,19 +100,17 @@ class EAXSToTagged():
             - message_el (lxml.etree._Element):
         """
 
-        tagged_eaxs_file = self.eaxs_file
-        ns_map = self.ns_map
         strip_html = self.strip_html
         nlp_tagger = self.nlp_tagger
         charset = self.charset
 
         # elements to alter.
         content_el, content_text = self._get_element(message_el, "Content") 
-        charset_el, charset_text = self._get_element(message_el, "Content", "us-ascii")
+        charset_el, charset_text = self._get_element(message_el, "Charset", "us-ascii")
         content_type_el, content_type_text = self._get_element(message_el, "ContentType",
                 "text/plain")
         transfer_encoding_el, transfer_encoding_text = self._get_element(message_el,
-                "Content")
+                "TransferEncoding")
 
         # stop if no content.
         if content_el is None or content_text is None:
@@ -139,41 +133,46 @@ class EAXSToTagged():
         return message_el
 
 
-    def tag_eaxs(self):
+    def get_tagged(self, eaxs_file):
         """ Writes file ... return filepath ... """
 
-        eaxs_file = self.eaxs_file
         ncdcr_uri = self.ncdcr_uri
-        ns_map = self.ns_map
-
-        #
-        account = "{" + ncdcr_uri + "}Account"
-        account_el = etree.Element(account, nsmap=ns_map)
 
         # 
-        parser = etree.XMLParser(strip_cdata=False, collect_ids=False)
+        parser = etree.XMLParser(strip_cdata=False)
         root = etree.parse(eaxs_file, parser).getroot()
 
         #
-        children = root.getchildren()#iterchildren()
+        children = root.iterchildren()
         for child_el in children:
             if child_el.tag == "{" + ncdcr_uri + "}Folder":
                 messages = self._get_messages(child_el)
                 for message_el in messages:
                     message_el = self._update_message(message_el)
-            
+
         return root
 
 
+    def write_tagged(self, eaxs_file, tagged_eaxs_file=None):
+        """ """
+        
+        charset = self.charset        
+        
+        if tagged_eaxs_file is None:
+            tagged_eaxs_file = eaxs_file.replace(".xml", "__tagged.xml")
+        
+        tagged_root = self.get_tagged(eaxs_file)
+        with etree.xmlfile(tagged_eaxs_file, encoding=charset) as xf:
+            xf.write_declaration()
+            xf.write(tagged_root, pretty_print=True)
+        
+
 # TEST.
-def main(eaxs_file, tagged_eaxs_file):
+def main(eaxs_file):
 
-    e2t = EAXSToTagged(eaxs_file, tagged_eaxs_file,  lambda x: x, lambda x: "FOOBAR")
-    tagged = e2t.tag_eaxs()
+    e2t = EAXSToTagged(lambda x: x, lambda x: "FOOBAR")
+    tagged = e2t.write_tagged(eaxs_file, "testTagged.xml")
 
-    with etree.xmlfile("foo.xml", encoding="UTF-8") as f:
-        f.write_declaration()
-        f.write(tagged, pretty_print=True)
 
 if __name__ == "__main__":
         
