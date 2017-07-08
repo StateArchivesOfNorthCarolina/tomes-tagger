a9e72204fcb0b70db4183197a64f5fa77f5d48c2
Nitin Arora <nitaro74@gmail.com>
2017-07-08 12:53:36 -0400

    Undid stoopid changes.

--- lib/text_to_nlp.py
+++ lib/text_to_nlp.py
@@ -16,27 +16,25 @@ class TextToNLP():
     around pycorenlp (https://github.com/smilli/py-corenlp). """
 
 
-    def __init__(self, host="http://localhost", port=9000, mapping_file="regexner_TOMES/mappings.txt",
-            *args, **kwargs):
+    def __init__(self, host="http://localhost", port=9000,
+            mapping_file="regexner_TOMES/mappings.txt", *args, **kwargs):
         """ Sets attributes. 
         
         Args:
             - port (int): ???
-            - timeout (int): ???
             - mapping_file (str): ???
             - *args/**kwargs: ???
         """
 
         # set annotation server and options.
-        self.port = str(port)
+        self.host = "{}:{}".format(host, port)
         self.mapping_file = mapping_file
-        self.host = ":".join([host, self.port])
         self.annotator = StanfordCoreNLP(self.host, *args, **kwargs)
         self.options = {"annotators": "tokenize, ssplit, pos, ner, regexner",
                 "outputFormat": "json",
                 "regexner.mapping": self.mapping_file}
 
-
+    
     def get_NLP(self, text):
         """ Returns Stanford CoreNLP results as dictionary.
         
