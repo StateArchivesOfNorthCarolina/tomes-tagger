88449ac3193ff6b5c9f51815319324f8bcffd422
Nitin Arora <nitaro74@gmail.com>
2017-09-19 15:32:13 -0400

    Making progress ...

--- lib/excel_to_stanford.py
+++ lib/excel_to_stanford.py
@@ -4,7 +4,8 @@
 This module ... ???
 
 Todo:
-    * ???
+    * Do you need to handle/skip blank rows?
+    * Can you assume the data is small enough to use a row list?
 
 """
 
@@ -15,7 +16,7 @@ import os
 from openpyxl import load_workbook
 
 
-class EXCELToSTANFORD):
+class ExcelToStanford():
     """ A class for ... ??? """
 
 
@@ -25,27 +26,29 @@ class EXCELToSTANFORD):
         
         # set logger; suppress logging by default. 
         self.logger = logging.getLogger(__name__)
-        self.logger.addHandler(logging.NullHandler()
+        self.logger.addHandler(logging.NullHandler())
 
         # ???
-        self.patter_header = patter_header
+        self.pattern_header = pattern_header
         self.case_header = case_header
         self.label_header = label_header
         self.authority_header = authority_header
         self.charset = charset
 
 
-    def _get_regex_pattern(self, pattern, case_sensitive):
+    def _get_pattern(self, pattern, case_sensitive):
         """ ??? """
-
-        # ???
-        replacements = {"{abc}":".*[a-zA-Z]", "{123}":".*[0-9]", "{abc123}":".*[a-zA-Z0-9]"}
-        for match, replacement in replacements.items():
-            pattern = pattern.replace(match, replacement)
 
         # ???
         if case_sensitive:
             pattern = "(?i)" + " (?i)".join(pattern.split())
+
+        # ???
+        replacements = {"{abc}":".*[a-zA-Z]", "{123}":".*[0-9]", "{abc123}":".*[a-zA-Z0-9]"}
+        for match, replacement in replacements.items():
+            pattern = pattern.replace("(?i)" + match, match)
+            pattern = pattern.replace(match, replacement)
+
 
         return pattern
 
@@ -53,26 +56,49 @@ class EXCELToSTANFORD):
     def _get_worksheet_data(self, ):
         """ ??? """
         
-        # can you convert "TRUE" to True/False here or does Excel library do that for me?
+        # ???
         return worksheet_data
 
 
-    def filter_worksheets(self, worksheets):
+    def filter_worksheets(self, xlsx_path):
         """ ??? """
 
+        workbook = load_workbook(xlsx_path, read_only=False, data_only=True)
+        worksheets = workbook.get_sheet_names()
+
+        for worksheet in worksheets:
+            if (self.pattern_header and 
+                self.case_header and
+                self.label_header and
+                self.authority_header) not in worksheet:
+                worksheets.remove(worksheet)
+            
         return worksheets
 
 
     def to_tsv(self, xslx_file, tsv_file=None):
         """ ??? """
         
+        # ???
         if os.path.isFile(tsv_file):
-            self.logger()
-            raise FileExistsError
+            err = "{} already exists.".format(tsv_file)
+            self.logger.error(err)
+            raise FileExistsError(err)
 
+        # ???
         with open(tsv_file, "w") as tsv:
+            tsv_writer = csv.writer(tsv, linetermator="")
+            
+            # ???; avoid trailing linebreak.
+            # Note: Stanford CoreNLP will error if a mapping file contains a trailing linebreak.
             for worksheet in self.filter_worksheets():
-                worksheet_data = self._get_worksheet_data(worksheet):
+                worksheet_rows = self._get_worksheet_rows(worksheet)
+                for worksheet_row in worksheet_rows[-1]: # if you're using a generator, you can't do this!
+                    tsv_writer.writerow(worksheet_row)
+                    tsv.write("\n")
+                tsv_writer.writerow(worksheet_rows[-1])
+
+        return
                 
 
 
