9e6612b116edbbb13552477687d6283239808c22
Nitin Arora <nitaro74@gmail.com>
2017-12-06 14:03:36 -0500

    Revert "Merge branch 'master' into nitin_dev"
    This reverts commit a62adbba61c2923514c2d01c4558bff62cc68816, reversing
    changes made to bd832947ad547c6e5105552898feb39d1fb9ccb4.

--- lib/html_to_text.py
+++ lib/html_to_text.py
@@ -8,13 +8,6 @@ import logging
 import os
 import subprocess
 from bs4 import BeautifulSoup
-import platform
-
-if platform.system() == "Windows":
-    lynx_exec = "C:\Program Files (x86)\lynx\lynx.exe"
-
-if platform.system() == "Linux":
-    lynx_exec = "lynx"
 
 
 class ModifyHTML():
@@ -119,7 +112,7 @@ class HTMLToText():
     '\nHello World!\n\n'
     """
     
-    def __init__(self, custom_options=None, temp_file=os.path.join(os.getcwd(), "_tmp.html")):
+    def __init__(self, custom_options=None, temp_file="_tmp.html"):
         """ Sets instance attributes.
         
         Args:
@@ -156,6 +149,7 @@ class HTMLToText():
                     self.temp_file))
                 pass
 
+
     def text(self, html, is_raw=False, charset="utf-8"):
         """ Converts HTML files OR strings to plain text string via the Lynx browser.
 
@@ -172,11 +166,8 @@ class HTMLToText():
 
         # create beginning Lynx command line snippet.
         arg_options = [key for key, val in self.options.items() if val]
-        args = []
-        args.append(lynx_exec)
-        for opt in arg_options:
-            args.append("-{}".format(opt))
-
+        args = "lynx -"
+        args += " -".join(arg_options)
 
         # if @is_raw == True, write @html to temporary file.
         # complete command line snippet.
@@ -185,24 +176,16 @@ class HTMLToText():
                     "Writing HTML to temporary HTML file: {}".format(self.temp_file))
             with codecs.open(self.temp_file, "w", encoding=charset) as tmp:
                 tmp.write(html)
-            #args += " " + self.temp_file
-            args.append(self.temp_file)
+            args += " " + self.temp_file
         else:
-            #args += " " + html
-            args.append(html)
+            args += " " + html
 
         # run Lynx.
         self.logger.debug("Converting HTML to text via: '{}'".format(args))
-        text = html
-        try:
-            cmd = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-            text = cmd.stdout.decode(encoding=charset, errors="backslashreplace")
-        except FileNotFoundError as e:
-            print(e)
-            self.logger.error(e)
-            self.logger.error(html)
+        cmd = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
 
         # return stdout.
+        text = cmd.stdout.decode(encoding=charset, errors="backslashreplace")
 
         return text
 
