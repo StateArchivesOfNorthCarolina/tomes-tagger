4aafe8816fd72840124f7264304eea6658323f2c
Nitin Arora <nitaro74@gmail.com>
2017-12-08 15:33:06 -0500

    Added docstrings; cleaned up quite a bit.

--- lib/text_to_nlp2_outline.py
+++ lib/text_to_nlp2_outline.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-""" This module converts ... 
+""" This module contains a class to extract tokens and their corresponding NER tags from a 
+given text using Stanford's CoreNLP. It also contains a class to wrap pycorenlp 
+(https://github.com/smilli/py-corenlp) and capture its exceptions more explicitly.
 
 Todo:
-
-    
+    * ???
 """
 
 # import modules.
@@ -14,9 +15,9 @@ import pycorenlp
 from textwrap import TextWrapper
 
 
-class _CoreNLP():
-    """ A private class to wrap pycorenlp (https://github.com/smilli/py-corenlp) and capture
-    its exceptions with a custom error. """
+class CoreNLP():
+    """ A class to wrap pycorenlp (https://github.com/smilli/py-corenlp) and capture its 
+    exceptions more explicitly. """
 
     def __init__(self, url, mapping_file="", tags_to_override=[], *args, **kwargs):
         """ Sets instance attributes.
@@ -25,8 +26,8 @@ class _CoreNLP():
             - url (str): The URL for the CoreNLP server (ex: "http://localhost:9003").
             - mapping_file (str): The relative path for the regexNER mapping file. This must
             be located within the CoreNLP server's file directory.
-            - tags_to_override (list): The CoreNLP NER tag values to override if they conflict
-            with a custom tag in @mapping_file. 
+            - tags_to_override (list): The CoreNLP NER tag values to override if they 
+            conflict with a custom tag in @mapping_file. 
             -*args/**kwargs: Any additional, optional arguments to pass to pycorenlp.
         """
 
@@ -81,18 +82,27 @@ class _CoreNLP():
 
 
     class Connection_Error(Exception):
-        """ A custom error to trap connection errors from pycorenlp. """
+        """ A custom error class for trapping connection errors from pycorenlp. """
         pass
 
 
 class TextToNLP():
-    """ ??? """
+    """ A class to extract tokens and their corresponding NER tags from a given text using
+    Stanford's CoreNLP. """
 
 
-    def __init__(self, host="http://localhost", port=9003, 
+    def __init__(self, host="http://localhost", port=9003, chunk_size=50000,
             mapping_file="regexner_TOMES/mappings.txt", tags_to_override=["LOCATION", 
                 "ORGANIZATION", "PERSON"]):
-        """ ??? """
+        """ Sets instance attributes.
+
+        Args:
+            - host (str): The base host URL for the CoreNLP server.
+            - port (int): The host's port on which to run the CoreNLP server.
+            - chunk_size (int): The maximum string length to send to  at a time.
+            - mapping_file (str): See help(CoreNLP) for more info.
+            - tags_to_override (list): See help(CoreNLP) for more info.
+            """
         
         # set logger; suppress logging by default. 
         self.logger = logging.getLogger(__name__)
@@ -104,20 +114,34 @@ class TextToNLP():
         self.url="{}:{}".format(host, port)
 
         # compose instance of CoreNLP wrapper class.
-        self.corenlp = _CoreNLP(self.url)
+        self.nlp = CoreNLP(self.url, mapping_file, tags_to_override)
 
    
     def get_ner(self, text):
-        """ ??? """
+        """ Performs tokenization and NER tagging on @text.
         
-        # prepare output containers.
+        Args:
+            - text (str): The text to tokenize and tag.
+            
+        Returns:
+            list: The return value.
+            The NER tagger results as a list of tuples.
+            The first item in the tuple is a token. The second and third items are the NER 
+            tag value and the trailing whitespace chracter, respectively. All items are 
+            strings.
+        """
+        
+        # prepare output container.
         ner = []
-        failed_ner = [None]
+
+        # pre-prepare data to use during failures.
+        failed_ner = []
+        failed_token_group = "", "", ""
         
         # get NER tags.
         try:
             results = self.corenlp.annotate(text)
-        except (TypeError, self.corenlp.Connection_Error) as e:
+        except (TypeError, self.nlp.Connection_Error) as e:
             self.logger.error(e)
             return failed_ner
 
@@ -127,7 +151,7 @@ class TextToNLP():
                 type(results).__name__))
             return failed_ner
         
-        # ensure @results contains required key.
+        # verify @results contains required key.
         if "sentences" not in results:
             self.logger.warning("CoreNLP response is missing required field 'sentences'.")
             return failed_ner
@@ -136,22 +160,37 @@ class TextToNLP():
         sentences = results["sentences"]
         for sentence in sentences:
 
+            # verify @sentence contains required key.
+            if "tokens" not in sentence:
+                msg = "No 'tokens' field not found; appending empty values to output."
+                self.logger.warning(msg)
+                ner.append(failed_token_group)
+                continue
+
+            # get values.
             tokens = sentence["tokens"]
             for token in tokens:
 
+                # determine key to use for original text.
+                text_key = "originalText"
+                if text_key not in token:
+                    text_key = "word"
+
+                # get values.
                 try:
-                    text = token["originalText"]
-                except KeyError:
-                    text = token["word"]
+                    text, tag, tspace = token[text_key], token["ner"], token["after"]
+                except KeyError as e:
+                    self.logger.error(e)
+                    self.logger.warning("Token data not found; appending empty values to \
+                            output.")
+                    text, tag, tspace = failed_token_group
                 
-                tag = token["ner"]
-                if tag == "O": # because CoreNLP uses "O" for a null NER tag.
-                    tag = None
-                
-                trailing_space = token["after"]
-                
-                # append values as tuple.
-                token_group = text, tag, trailing_space
+                # overwrite CoreNLP's use of "O" for a null NER tag.
+                if tag == "O":
+                    tag = ""
+
+                # append final values to @ner.
+                token_group = text, tag, tspace
                 ner.append(token_group)
 
         return ner
@@ -161,6 +200,6 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=logging.WARNING)
     t2n = TextToNLP()
-    res = t2n.get_ner("1234 is a number. Jane Doe is married to John Doe.")
-    for w, t, s in res:
-        print("{} [{}]".format(w, t))
+    res = t2n.get_ner("1 Jane Doe.")
+    for r in res:
+        print(r)
