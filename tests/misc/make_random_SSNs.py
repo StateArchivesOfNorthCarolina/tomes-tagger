a083eb5a49f25540a8feac0dad0177fda7a6de82
Nitin Arora <nitaro74@gmail.com>
2018-01-16 11:27:35 -0500

    Cleaned up docstrings.

--- tests/misc/make_random_SSNs.py
+++ tests/misc/make_random_SSNs.py
@@ -1,47 +1,65 @@
 #!/usr/bin/env/python3
 
-""" Makes random SSN numbers for testing; writes to file. """
+""" Creates file with random social-security-like numbers for testing. That is to say the
+numbers may or may not meet the SSN specification. """
 
 # import modules.
 import os
-import sys
 from random import randint
 
-# set output file.
-output = "PII.social_security_number__testData.txt"
 
-# prevent overwrite of existing test data file.
-if os.path.isfile(output):
-	msg = "\nCannot continue.\n{} already exists.\n".format(output)
-	sys.exit(msg)
-	
-# make SSN.
-def makessn():
-	temp = []
-	def genssn(l=3):
-		nums = ""
-		for i in range(0,l):
-			nums += str(randint(0,9))
-		temp.append(nums)
-	genssn(3)
-	genssn(2)
-	genssn(4)
-	hyphen = randint(0,2)
-	if hyphen == 0:
-		sep = "-"
-	elif hyphen == 1:
-		sep = " "
-	else:
-		sep = ""
-	ssn = sep.join(temp)
-	return ssn
+def genSSN():
+    """ Returns a random SSN-like number. """
 
-# call makessn() and write SSNs to file.
-with open(output, "w") as f:
-	for i in range(0,20):
-		f.write(makessn())
-		f.write("\n")
-		# if randint(0,2) > 0:
-			# f.write("\n")
-		# else:
-			# f.write(" ")
+    temp = []
+    
+    # generate random number with a given @length; append it (as string) to @temp.
+    def genNum(length=3):
+            nums = ""
+            for i in range(0,length):
+                nums += str(randint(0,9))
+            temp.append(nums)
+    
+    # make random parts of SSN. 
+    genNum(3)
+    genNum(2)
+    genNum(4)
+
+    # randomly add a hyphen, whitespace, or nothing as the separator.
+    hyphen = randint(0,2)
+    if hyphen == 0:
+        sep = "-"
+    elif hyphen == 1:
+        sep = " "
+    else:
+        sep = ""
+    
+    ssn = sep.join(temp)
+    return ssn
+
+
+def makeSSNFile(output="random_SSN_numbers.txt", total=20):
+    """ Outputs @total random SSN-like numbers to @output file. """
+
+    # prevent overwrite of exixting file. 
+    if os.path.isfile(output):
+        raise FileExistsError("Can't overwrite '{}'; please delete or rename it.".format(
+            output))
+
+    # write SSNS to @output.
+    with open(output, "w") as out:
+        
+        for i in range(0,20):
+            out.write(genSSN())
+            out.write("\n")
+            # if randint(0,2) > 0:
+                # out.write("\n")
+            # else:
+                # out.write(" ")
+
+    return
+
+
+if __name__ == "__main__":
+    makeSSNFile()
+
