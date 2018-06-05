43479331c788266b7e70cff254501df175a9b382
Nitaro <nitaro74@gmail.com>
2018-06-05 16:51:33 -0400

    Removed import.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, setuptools
-from tomes_tagger.tagger import __NAME__, __FULLNAME__, __DESCRIPTION__, __URL__, __VERSION__,
-__AUTHOR__, __AUTHOR_EMAIL__
+from tomes_tagger.tagger import __NAME__, __DESCRIPTION__, __URL__, __VERSION__, __AUTHOR__, __AUTHOR_EMAIL__
 
 def doc():
     with open("docs/documentation.md") as d:
